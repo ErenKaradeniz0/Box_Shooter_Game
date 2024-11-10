56fb41f1534c95f5b5cc210147507e7a6d543e72
svvlgr <200101032@ogr.atu.edu.tr>
2024-11-10 14:35:25 +0300

    final

--- Box_Shooter_Game/Main.cpp
+++ Box_Shooter_Game/Main.cpp
@@ -1,9 +1,12 @@
 #include "icb_gui.h"
 
 // Globals
+/*
+5. Note that the use of global variables is restricted, except for the designated ICBYTES
+object and the keypressed variable.
+*/
 int keypressed;
 ICBYTES screenMatrix;
-HANDLE gameThreads[5] = { NULL }; // Store thread handles
 
 struct GameObject {
     int x, y;
@@ -15,220 +18,239 @@ struct GameObject {
 
 struct ThreadParams {
     GameObject ship;
-    GameObject alien;
+    GameObject box;
     GameObject bullet;
     int FRM1;
-    bool gameRunning;
+    bool* gameRunning;
+    bool& isGameRunning() {
+        return *gameRunning;
+    }
 };
 
-ThreadParams* currentGame = nullptr;
-
-void CleanupThreads() {
-    for (int i = 0; i < 5; i++) {
-        if (gameThreads[i] != NULL) {
-            TerminateThread(gameThreads[i], 0);
-            CloseHandle(gameThreads[i]);
-            gameThreads[i] = NULL;
-        }
-    }
-}
-
+// Create window
 void ICGUI_Create() {
     ICG_MWTitle("Space Shooter");
-    ICG_MWSize(700, 700);
+    ICG_MWSize(540, 600);
 }
 
-void DrawExplosion(GameObject* obj, ThreadParams* params) {
-    FillRect(screenMatrix, params->alien.x, params->alien.y,
-        params->alien.width, params->alien.height, 0);
 
+void DrawExplosion(GameObject* obj, ThreadParams* params) {
+    //Draw Explosion
     if (obj->explosionFrame > 10) {
         obj->isAlive = false;
         obj->explosionType = 0;
         obj->explosionFrame = 0;
-        FillRect(screenMatrix, obj->x, obj->y, params->alien.width, params->alien.height, 0x000000);
+        FillRect(screenMatrix, obj->x, obj->y, params->box.width, params->box.height, 0x000000); // Patlama alanýný temizle
         return;
     }
 
-    FillRect(screenMatrix, obj->x, obj->y, params->alien.width, params->alien.height, 0x000000);
+
+    int color = 0xFFFF00; // Explosion Color Yellow
+    FillRect(screenMatrix, obj->x, obj->y, params->box.width, params->box.height, color);
+
+    // Explosion Middle
+    if (obj->explosionType == 2) {
+        FillRect(screenMatrix, obj->x + ((params->box.width - (obj->explosionFrame * (params->box.width / 10))) / 2), obj->y, obj->explosionFrame * (params->box.width / 10), params->box.height, 0x000000);
+        FillRect(screenMatrix, obj->x, obj->y + ((params->box.height - (obj->explosionFrame * (params->box.width / 10))) / 2), params->box.width, obj->explosionFrame * (params->box.height / 10), 0x000000);
+    }
+    // Case 1 Case 3  diagonally move Case 2 explosion 
     switch (obj->explosionType) {
     case 1:
-        obj->x = obj->x + 8;
-        obj->y = obj->y - 8;
+        obj->x += 8;
+        obj->y -= 8;
         break;
     case 2:
-        obj->y = obj->y - 8;
+        obj->y -= 8;
         break;
     case 3:
-        obj->x = obj->x - 8;
-        obj->y = obj->y - 8;
+        obj->x -= 8;
+        obj->y -= 8;
         break;
     }
-    FillRect(screenMatrix, obj->x, obj->y, params->alien.width, params->alien.height, 0xFFFF00);
+
+
+
     obj->explosionFrame++;
 }
 
-void AnimationThread(ThreadParams* params) {
-    while (params->gameRunning) {
-        if (params == currentGame) { // Only render if this is current game
-            DisplayImage(params->FRM1, screenMatrix);
-            Sleep(30);
-        }
-        else {
-            break;
-        }
-    }
 
-    if (params == currentGame) { // Only show game over if this is current game
-        screenMatrix = 0x000055;
-        ICG_SetFont(50, 0, "Arial");
-        Impress12x20(screenMatrix, 275, 300, "GAME OVER", 0xFFFFFF);
-        DisplayImage(params->FRM1, screenMatrix);
-    }
-}
+// Drawing Thread
+void DrawThread(ThreadParams* params) {
+    while (params->isGameRunning()) {
+        // Clear screen
+        screenMatrix = 0;
 
-void ShipThread(ThreadParams* params) {
-    while (params->gameRunning && params == currentGame) {
-        // Delete old position
-        FillRect(screenMatrix, params->ship.x, params->ship.y,
-            params->ship.width, params->ship.height, 0);
-
-        // Move Ship
-        if (keypressed == 37) params->ship.x = max(0, params->ship.x - 10);
-        if (keypressed == 39) params->ship.x = min(610, params->ship.x + 10);
-
-        // Print new position
+        // Draw ship
         FillRect(screenMatrix, params->ship.x, params->ship.y,
             params->ship.width, params->ship.height, 0xFF0000);
 
-        Sleep(10);
+        // Draw box or explosion
+        if (params->box.isAlive) {
+            FillRect(screenMatrix, params->box.x, params->box.y,
+                params->box.width, params->box.height, 0x00FF00);
+        }
+        else if (params->box.explosionType > 0) {
+            DrawExplosion(&params->box, params);
+        }
+
+        // Draw bullet
+        if (params->bullet.isAlive) {
+            FillRect(screenMatrix, params->bullet.x, params->bullet.y,
+                params->bullet.width, params->bullet.height, 0x0000FF);
+        }
+
+        DisplayImage(params->FRM1, screenMatrix);
+        Sleep(30);
     }
 }
 
-void AlienThread(ThreadParams* params) {
-    while (params->gameRunning && params == currentGame) {
-        if (!params->alien.isAlive) {
-            params->alien.x = rand() % 580;
-            params->alien.y = 0;
-            params->alien.isAlive = true;
-            params->alien.explosionType = 0;
-            params->alien.explosionFrame = 0;
+
+void ShipThread(ThreadParams* params) {
+    while (params->isGameRunning()) {
+        // Move Ship
+        if (keypressed == 37) params->ship.x = max(0, params->ship.x - 10);
+        if (keypressed == 39) params->ship.x = min(460, params->ship.x + 10);
+        keypressed = 0;
+        Sleep(30);
+    }
+}
+
+void BoxThread(ThreadParams* params) {
+    while (params->isGameRunning()) {
+        if (!params->box.isAlive && params->box.explosionType == 0) {
+            // Respawn box
+            params->box.x = rand() % 430;
+            params->box.y = 0;
+            params->box.isAlive = true;
+            params->box.explosionType = 0;
+            params->box.explosionFrame = 0;
         }
+        // Move box
+        else if (params->box.isAlive) {
+            params->box.y += 4;
+            // Collision to ship or cross to border
+            if (params->box.y > params->ship.y ||
+                (params->box.y + params->box.height >= params->ship.y &&
+                    params->box.x + params->box.width >= params->ship.x &&
+                    params->box.x <= params->ship.x + params->ship.width)) {
 
-        if (params->alien.explosionType > 0) {
-            Sleep(10);
-            DrawExplosion(&params->alien, params);
-        }
-        else {
-            FillRect(screenMatrix, params->alien.x, params->alien.y,
-                params->alien.width, params->alien.height, 0);
+                //delete objects
+                params->box.isAlive = false;
+                params->ship.isAlive = false;
+                params->bullet.isAlive = false;
 
-            params->alien.y += 4;
+                // GameOver flag
+                *(params->gameRunning) = false;
+                Sleep(50);
+                // GAME OVER Screen
+                screenMatrix = 0x000055;
+                ICG_SetFont(50, 0, "Arial");
+                Impress12x20(screenMatrix, 200, 250, "GAME OVER", 0xFFFFFF);
+                DisplayImage(params->FRM1, screenMatrix);
 
-            if (params->alien.y > 600 ||
-                (params->alien.y + params->alien.height >= params->ship.y &&
-                    params->alien.x + params->alien.width >= params->ship.x &&
-                    params->alien.x <= params->ship.x + params->ship.width)) {
-                params->gameRunning = false;
-                params->alien.isAlive = false;
-                continue;
+
+
             }
-
-            FillRect(screenMatrix, params->alien.x, params->alien.y,
-                params->alien.width, params->alien.height, 0x00FF00);
         }
-
-        Sleep(10);
+        Sleep(30);
     }
 }
 
 void BulletThread(ThreadParams* params) {
-    while (params->gameRunning && params == currentGame) {
+    while (params->isGameRunning()) {
+        // Create Bullet
         if (keypressed == 32 && !params->bullet.isAlive) {
             params->bullet.x = params->ship.x + (params->ship.width / 2) - (params->bullet.width / 2);
             params->bullet.y = params->ship.y - params->bullet.height;
             params->bullet.isAlive = true;
             keypressed = 0;
         }
-
+        // Move Bullet
         if (params->bullet.isAlive) {
-            FillRect(screenMatrix, params->bullet.x, params->bullet.y,
-                params->bullet.width, params->bullet.height, 0);
-
             params->bullet.y -= 10;
+            if (params->box.isAlive &&
+                params->bullet.y <= params->box.y + params->box.height &&
+                params->bullet.y + params->bullet.height >= params->box.y &&
+                params->bullet.x + params->bullet.width >= params->box.x &&
+                params->bullet.x <= params->box.x + params->box.width) {
 
-            if (params->alien.isAlive &&
-                params->bullet.y <= params->alien.y + params->alien.height &&
-                params->bullet.y + params->bullet.height >= params->alien.y &&
-                params->bullet.x + params->bullet.width >= params->alien.x &&
-                params->bullet.x <= params->alien.x + params->alien.width) {
 
-                int hitX = params->bullet.x - params->alien.x;
-                if (hitX < params->alien.width / 3)
-                    params->alien.explosionType = 1;
-                else if (hitX < (params->alien.width * 2) / 3)
-                    params->alien.explosionType = 2;
-                else
-                    params->alien.explosionType = 3;
+                int hitX = params->bullet.x - params->box.x;
+                /*
+                2. When a bullet strikes the left side of a box, specifically within the leftmost 3-unit
+                region, the box will move to the top-right corner.
+                */
+                int leftPart = params->box.width / 3 - 1;
 
+                /*
+                4. A bullet that hits the central 4-unit region of the box will result in the box being
+                destroyed.
+                */
+                int middlePart = params->box.width - leftPart;
+                if (hitX <= leftPart) //3*x*k
+
+                    params->box.explosionType = 1;
+                else if (hitX <= middlePart) //4*x*k
+                    params->box.explosionType = 2;
+
+                /*
+                3. If a bullet hits the right side of the box, within the rightmost 3-unit region, the box
+                will instead move to the top-left corner.
+                */
+                else  //3*x*k
+                    params->box.explosionType = 3;
+
+                params->box.isAlive = false;
                 params->bullet.isAlive = false;
-                continue;
             }
-
+            // Screen overflow check
             if (params->bullet.y < 0) {
                 params->bullet.isAlive = false;
-                continue;
             }
-
-            FillRect(screenMatrix, params->bullet.x, params->bullet.y,
-                params->bullet.width, params->bullet.height, 0x0000FF);
         }
-
-        Sleep(10);
+        Sleep(30);
     }
 }
 
-void StartGame() {
-    // Cleanup any existing game threads
-    if (currentGame != nullptr) {
-        currentGame->gameRunning = false;
-        CleanupThreads();
-        delete currentGame;
-    }
+void StartGame(void* gameRunning) {
+    bool* gameRunningPtr = (bool*)gameRunning;
+    if (*gameRunningPtr) return;
 
+    *gameRunningPtr = true;
     // Reset the screen
     screenMatrix = 0;
 
-    // Create new game state
-    currentGame = new ThreadParams{
-        {300, 580, 60, 20, true, 0, 0},      // ship
-        {rand() % 580, 0, 40, 40, true, 0, 0},  // alien
-        {0, 0, 10, 20, false, 0, 0},         // bullet
-        ICG_FrameMedium(5, 40, 700, 700),    // frm1
-        true                                  // gameRunning
+    // Define ThreadParams
+    int gameScreenX = 500, GameScreenY = 500;
+    int shipX = 250, shipY = 485;
+    int boxSize = 40;
+    int bulletWidth = 2, bulletHeight = 10;
+
+    ThreadParams* params = new ThreadParams{
+        {shipX, shipY, 40, 10, true, 0, 0},
+        {rand() % (gameScreenX - boxSize) , 0, boxSize, boxSize, true, 0, 0},
+        {0, 0, bulletWidth, bulletHeight, false, 0, 0},
+        ICG_FrameMedium(5, 40, 1, 1),
+        gameRunningPtr
     };
 
-    // Start new threads
-    gameThreads[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)AnimationThread, currentGame, 0, NULL);
-    gameThreads[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ShipThread, currentGame, 0, NULL);
-    gameThreads[2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)AlienThread, currentGame, 0, NULL);
-    gameThreads[3] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)BulletThread, currentGame, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DrawThread, params, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ShipThread, params, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)BoxThread, params, 0, NULL);
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)BulletThread, params, 0, NULL);
 
     SetFocus(ICG_GetMainWindow());
 }
 
 void WhenKeyPressed(int k) {
-    if (currentGame && currentGame->gameRunning) {
-        keypressed = k;
-        Sleep(30);
-        keypressed = 0;
-    }
+    keypressed = k;
 }
 
 void ICGUI_main() {
-    CreateImage(screenMatrix, 700, 700, ICB_UINT);
+    CreateImage(screenMatrix, 500, 500, ICB_UINT);
     screenMatrix = 0;
 
-    ICG_Button(5, 5, 120, 25, "START GAME", StartGame);
+    static bool gameRunning = false;
+    ICG_Button(5, 5, 120, 25, "START GAME", StartGame, &gameRunning);
     ICG_SetOnKeyPressed(WhenKeyPressed);
-}
+},
